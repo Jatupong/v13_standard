98f602dab0d38cbd3a9aebdd8d433b90641858ab
ITAAS <ajatupong@hotmail.com>
2021-04-26 14:22:34 +0700

    mrp costing

--- itaas_mrp_product_costing/__manifest__.py
+++ itaas_mrp_product_costing/__manifest__.py
@@ -2,33 +2,28 @@
 # Copyright (c) Open Value All Rights Reserved
 
 {
-'name': 'MRP Product Costing',
+'name': 'ITAAS MRP Product Costing',
  'summary': 'MRP Product Costing',
- 'version': '13.4.0.0.0',
+ 'version': '13.0.1.0',
  'category': 'Manufacturing',
-  "website": 'linkedin.com/in/openvalue-consulting-472448176',
-  'author': "Open Value",
-  'support': 'info.openvalue@gmail.com',
+  "website": 'www.itaas.co.th',
+  'author': "ITAAS",
+  'support': 'info@itaas.co.th',
   'license': "Other proprietary",
-  'price': 400.00,
-  'currency': 'EUR',
     "depends": [
         'stock_account',
         'purchase',
         'mrp',
         'account',
         'analytic',
-        'mrp_shop_floor_control',
         'mrp_workorder',
+        'itaas_create_stock_account',
     ],
     "data": [
-        'views/account_move_line_views.xml',
-        'views/account_analytic_line_views.xml',
-        'views/mrp_product_costing_parameters.xml',
-        'views/mrp_workcenter_views.xml',
-        'views/mrp_bom_views.xml',
+'views/account_move_line_views.xml',
         'views/mrp_production_views.xml',
-        'views/mrp_product_costing_views.xml',
+
+
     ],
   'application': False,
   'installable': True,
